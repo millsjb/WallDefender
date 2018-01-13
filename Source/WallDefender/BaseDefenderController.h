fdb65b1e3d454c6b4c0402211331a77ee08f2cc2
Peter Keyser <keysertheloner@gmail.com>
2018-01-13 18:35:54 -0500

    player character/controller added

---
+++ Source/WallDefender/BaseDefenderController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BaseDefenderController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WALLDEFENDER_API ABaseDefenderController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
